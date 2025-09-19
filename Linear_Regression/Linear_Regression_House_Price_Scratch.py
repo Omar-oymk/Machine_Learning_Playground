1fe870fe7bfdf6dc432871ee96e168fd227a16f2
omaroymk <72468295+oymk0@users.noreply.github.com>
2025-09-19 15:22:19 +0300

    Added visualization and verbose modes and optimized things a little

--- Linear_Regression/Linear_Regression_House_Price_Scratch.py
+++ Linear_Regression/Linear_Regression_House_Price_Scratch.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
-
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 # load dataset
 df = pd.read_csv(r"C:\Users\user\Downloads\archive (1)\house_price_regression_dataset.csv")
@@ -14,16 +15,14 @@ def ZScoreStandardization(X):
     return (X - X.mean()) / X.std()
 
 ## correlation to remove redundant features
-import matplotlib.pyplot as plt
-import seaborn as sns
-
 sns.heatmap(df.corr().abs(), cmap="coolwarm")
 plt.show()
 
 ### from that we can see that it is highly with lot_size, garage_size and year_built
 ### however the lot_size is kinda highly corr with year_built so we will just use the lot_size for the sake of simplicity
 
-X = df[["Lot_Size", "Garage_Size"]]
+# X = df[["Lot_Size", "Garage_Size"]]
+X = df[["Lot_Size"]]
 X = ZScoreStandardization(X)
 Y = df["House_Price"]
 Y_mean = Y.mean()
@@ -46,9 +45,9 @@ def train_test_split(X, Y, train_size = None, test_size = None, shuffle = False,
     n = (int)(train_size * len(X))
     if (shuffle):
         np.random.seed(random_state)
-        indicies_shuffled = np.random.permutation(len(X))
-        shuffled_x = X.iloc[indicies_shuffled]
-        shuffled_y = Y.iloc[indicies_shuffled]
+        indices_shuffled = np.random.permutation(len(X))
+        shuffled_x = X.iloc[indices_shuffled]
+        shuffled_y = Y.iloc[indices_shuffled]
         x_train, x_test, y_train, y_test = shuffled_x[:n], shuffled_x[n:], shuffled_y[:n], shuffled_y[n:]
     else:
         x_train, x_test, y_train, y_test = X[:n], X[n:], Y[:n], Y[n:]
@@ -59,59 +58,73 @@ def train_test_split(X, Y, train_size = None, test_size = None, shuffle = False,
 #region Model Training
 
 class LinearRegression:
-    
-    def __init__(self, epochs = 10000, learning_rate = 0.001):
+    """VISUALIZATION WORKS ONLY IF X HAS ONLY 1 FEATURE"""
+    def __init__(self, epochs = 10000, learning_rate = 0.001, verbose = False, visualization = False):
         self.epochs = epochs
         self.learning_rate = learning_rate
+        self.verbose = verbose
+        self.visualization = visualization
         self.coef_ = None
         self.bias_ = None
 
-    def Fit(self, x_train, y_train):
+    def fit(self, x_train, y_train):
+        x_columns = x_train.columns
+        label = y_train.name
         x_train = x_train.values
         y_train = y_train.values
-        self.coef_, self.bias_ = self._InitializeWeights(x_train)
+        self.coef_, self.bias_ = self._initialize_weights(x_train)
 
+        if(x_train.shape[1] != 1): print("VISUALIZTION WORKS ON ONLY 1 FEATURE")
         for i in range(self.epochs):
             if (i % 1000 == 0): 
-                print("---------------------------------------------")
-                print(f"MSE = {self._CostFunction(x_train, y_train)}")
-                print(f"RMSE = {np.sqrt(self._CostFunction(x_train, y_train))}")
-                print("---------------------------------------------")
+                if(self.verbose):
+                    print("---------------------------------------------")
+                    print(f"MSE = {self._cost_function(x_train, y_train)}")
+                    print(f"RMSE = {np.sqrt(self._cost_function(x_train, y_train))}")
+                    print("---------------------------------------------")
+                if(self.visualization):
+                    if(x_train.shape[1] == 1):
+                        plt.title("Training Progress")
+                        plt.scatter(x_train, y_train, color = "red", label = "True values")
+                        plt.plot(x_train, self._linear(x_train), "b--", label = "Model Fitting")
+                        plt.xlabel(x_columns[0])
+                        plt.ylabel(label)
+                        plt.legend()
+                        plt.show()
+            self._gradient_descent(x_train, y_train)
 
-            self._GradientDescent(x_train, y_train)
 
-
-    def _InitializeWeights(self, x_train):
+    def _initialize_weights(self, x_train):
         self.coef_ = np.zeros(x_train.shape[1])
         self.bias_ = 0
         return self.coef_, self.bias_
 
-    def _Linear(self, x_train):
+    def _linear(self, x_train):
         return np.dot(x_train, self.coef_) + self.bias_
 
-    def _CostFunction(self, x_train, y_train):
+    def _cost_function(self, x_train, y_train):
         # mse fn 
-        return (1/x_train.shape[0]) * np.sum((self._Linear(x_train) - y_train)**2)
+        return (1/x_train.shape[0]) * np.sum((self._linear(x_train) - y_train)**2)
 
-    def _Gradient(self, x_train, y_train):
+    def _gradient(self, x_train, y_train):
         # dCostdWeights = -(1/x_train.shape[0]) * 2 * np.sum(x_train * (self._Linear(x_train) - y_train))     # this will only work if it were for 1 feature
-        dCostdWeights = (1/x_train.shape[0]) * 2 * np.dot(x_train.T, (self._Linear(x_train) - y_train))
-        dCostdBias = (1/x_train.shape[0]) * 2 * np.sum((self._Linear(x_train) - y_train))
+        dCostdWeights = (1/x_train.shape[0]) * 2 * np.dot(x_train.T, (self._linear(x_train) - y_train))
+        dCostdBias = (1/x_train.shape[0]) * 2 * np.sum((self._linear(x_train) - y_train))
         return dCostdWeights, dCostdBias
     
-    def _GradientDescent(self, x_train, y_train):
-        dCostWeights, dcostBias = self._Gradient(x_train, y_train)
+    def _gradient_descent(self, x_train, y_train):
+        dCostWeights, dcostBias = self._gradient(x_train, y_train)
         self.coef_ = self.coef_ - self.learning_rate * dCostWeights
         self.bias_ = self.bias_ - self.learning_rate * dcostBias
 
-    def Predict(self, x_test):
-        return self._Linear(x_test) * Y_std + Y_mean
+    def predict(self, x_test):
+        return self._linear(x_test) * Y_std + Y_mean
     
 #endregion
 
 #region main.py
 x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size = 0.2, shuffle = True, random_state = 42)
-model = LinearRegression()
-model.Fit(x_train, y_train)
-print(model.Predict(x_test))
+model = LinearRegression(visualization= True)
+model.fit(x_train, y_train)
+print(model.predict(x_test))
 #endregion
