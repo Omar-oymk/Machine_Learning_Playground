3c9b08ef8ff8bfed47ef4473a04084b066845af7
omaroymk <72468295+oymk0@users.noreply.github.com>
2025-09-20 20:14:39 +0300

    Added train_test_split fn from scratch

--- Decision_Trees/Decision_Tree_Pokemon_Scratch.py
+++ Decision_Trees/Decision_Tree_Pokemon_Scratch.py
@@ -37,7 +37,31 @@ print(df.corr())
 # for simplicity's sake since this is an implemenatation from scratch ill only keep
 # the total which is corr by approx 0.5
 X = [[df["Total"]]]
-Y = [df["Legendary"]]
+y = [df["Legendary"]]
 
+#endregion
+
+#region splitting data
+def train_test_split(X, y, train_size = None, test_size = None, shuffle = False, random_state = None):
+    if(train_size is not None and test_size is not None):
+        raise ValueError("NOT ALLOWED TO DEFINE BOTH train_size AND test_size AT THE SAME TIME")
+    
+    if(train_size is None and test_size is None):
+        train_size = 0.75
+        test_size = 0.25
+    elif(train_size is not None and 0 <= train_size <= 1): test_size = 1 - train_size
+    elif(test_size is not None and 0 <= test_size <= 1): train_size = 1 - test_size
+    else: raise ValueError("Please keep train/test size a fractional value between 0 and 1 inclusive")
+
+    n = (int)(train_size * len(X))
+    if(shuffle or random_state is not None):
+        np.random.seed(random_state)
+        indices_shuffled = np.random.permutation(len(X))    # the random.perm returns a shuffled n indices from 0 - n-1
+        X = X.iloc[indices_shuffled]
+        y = y.iloc[indices_shuffled]
+
+    return X.iloc[:n], X.iloc[n:], y.iloc[:n], y.iloc[n:]
+
+x_train, x_test, y_train, y_test = train_test_split(X, y, random_state = 42)
 #endregion
 
